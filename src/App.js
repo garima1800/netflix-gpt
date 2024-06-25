49f8f4457dcd201bf29f0765411504d548c66a45
unknown <garimabarwal12@gmail.com>
2024-06-25 12:51:14 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-800">
+      Learn react with Garima
+     
+    </div>
+  );
+}
+
+export default App;
